69ace68a65119f5cd859079f3d37df4bc9d7d59d
Chymme <chymme>
2022-09-05 23:22:57 +0400

    Added a function for computer play

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let computerScore = playerScore = 0
+
+function getComputerChoice() {
+    const rps = ['rock', 'paper', 'scissors']
+    const randomIndex = Math.floor(Math.random() * 3)
+    const computer = rps[randomIndex]
+
+    return computer
+}
+
+function rounds(computer, player) {
+    if (computer === player) {
+        return 'Tie';
+        
+    } else if (computer === 'rock' && player === 'scissors') {
+        computerScore++
+        return 'Computer wins';
+    } else if (computer === 'rock' && player === 'paper') {
+        playerScore++
+        return 'Player wins';
+    } else if (computer === 'paper' && player === 'scissors') {
+        ComputerScore++
+        return 'Player wins';
+    } else if (computer === 'paper' && player === 'rock') {
+        playerScore++
+        return 'Player wins';
+    } else if (computer === 'scissors' && player === 'paper') {
+        computerScore++
+        return 'Computer wins';
+    } else if (computer === 'scissors' && player === 'rock') {
+        PlayerScore++
+        return 'Player wins wins';
+    }
+  }
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+       const player = prompt('Rock, Paper, Or Scissors?').trim().toLowerCase()
+       const computer = getComputerChoice()
+       console.log(rounds(computer, player))
+    }
+
+  if (computerScore > playerScore) {
+    return 'The Computer beat you'
+  } else if (computerScore < playerScore) {
+    return 'You beat the computer'
+  } else {
+    return 'You and computer Tied'
+  }
+
+  }
+console.log(game());
+
+
+
